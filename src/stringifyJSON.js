ebdb2fb933041b29438edd08a36b3a3c182eb798
Hou <kchia87@gmail.com>
2014-11-04 22:16:08 -0800

    Implements stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,28 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  result = '';
+  if (Array.isArray(obj)){
+      for(var i = 0; i < obj.length; i++){
+        result += stringifyJSON(obj[i]);
+        if (i != obj.length - 1){
+          result += ','
+        }
+      }
+    return '[' + result + ']';
+  } else if(typeof obj === 'number'|| typeof obj === 'boolean'){
+        result += obj;
+  } else if(typeof obj === 'string'){
+        result += '"' + obj + '"';
+  } else if(obj === null){
+        result += 'null';
+  } else {
+      for(var key in obj){
+        result += '"'+ key + '"' + ':' + stringifyJSON(obj[key]) + ',';
+        }
+        return '{' + result.slice(0,result.length-1) + '}';
+  }
+  return result;
 };
+
